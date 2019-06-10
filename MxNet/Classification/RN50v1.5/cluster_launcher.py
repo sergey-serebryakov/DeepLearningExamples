485c104fddd7799a72d571400b526265f41967d0
Sergey Serebryakov <sergey.serebryakov@hpe.com>
2019-06-10 15:44:57 -0700

    Updates to CLI.

--- MxNet/Classification/RN50v1.5/cluster_launcher.py
+++ MxNet/Classification/RN50v1.5/cluster_launcher.py
@@ -40,26 +40,34 @@ def parse_args():
         """Return true if val represents true  value."""
         return val.lower() in ('true', 'on', 't', '1')
     parser = argparse.ArgumentParser()
-    parser.add_argument('--rendezvous', type=str, default='')
-    parser.add_argument('--num_workers', type=int, default=1)
-    parser.add_argument('--num_servers', type=int, default=-1)
 
-    parser.add_argument('--scheduler', nargs='?', const=True, default=False, type=str2bool)
-    parser.add_argument('--server', nargs='?', const=True, default=True, type=str2bool)
+    parser.add_argument('--rendezvous', type=str, default='',
+                        help='Scheduler rendezvous address IP:PORT.')
 
+    parser.add_argument('--host-ip', type=str, default=None,
+                        help='Network IP to use for this host.')
+    parser.add_argument('--host-interface', type=str, default=None,
+                        help='Network interface to use if host ip not provided.')
 
-    
-    parser.add_argument('--ps-verbose', type=str, default=-1)
+    parser.add_argument('--num_workers', type=int, default=1,
+                        help='Number of worker processes.')
+    parser.add_argument('--num_servers', type=int, default=-1,
+                        help='Number of server processes. By default, equals to number of workers.')
+
+    parser.add_argument('--scheduler', nargs='?', const=True, default=False, type=str2bool,
+                        help='If a scheduler needs to be run on this node.')
+    parser.add_argument('--server', nargs='?', const=True, default=True, type=str2bool,
+                        help='If a server needs to be run on this node.')
+
+    parser.add_argument('--ps-verbose', type=int, default=0)
     parser.add_argument("benchmark_script", type=str)
     parser.add_argument('benchmark_args', nargs=argparse.REMAINDER)
-    arg = parser.parse_args()
+    args = parser.parse_args()
 
     if args.num_servers < 0:
         args.num_servers = args.num_workers
     elif args.num_servers == 0:
         args.server = False
-    if args.ps_verbose < 0:
-        args.ps_verbose = int(os.environ.get('PS_VERBOSE', 0))
 
     return args
 
@@ -75,32 +83,33 @@ class Cluster(object):
     def __init__(self, args=None):
         if args is not None:
             self.num_workers = args.num_workers
-            self.num_servers = args.num_servers:
+            self.num_servers = args.num_servers
             self.is_scheduler = args.scheduler
             self.is_server = args.server
+            self.host_ip = args.host_ip
+            self.host_interface = args.host_interface
             self.ps_verbose_level = args.ps_verbose
             self.benchmark = {'script': args.benchmark_script, 'args': args.benchmark_args}
 
             scheduler = args.rendezvous.split(':')
             if len(scheduler) == 2:
-                self.scheduler = {'interface': None, 'host': scheduler[0], 'port': scheduler[1]}
-            elif len(scheduler) == 3:
-                self.scheduler = {'interface': scheduler[0], 'host': scheduler[1], 'port': scheduler[2]}
+                self.scheduler = {'host': scheduler[0], 'port': int(scheduler[1])}
             else:
                 raise ValueError("Invalid rendezvous specifier format (%s)" % args.rendezvous)
-        self.agents = {
-            'scheduler': None, 'server': None, 'worker': None
-        }
+        self.agents = {}
 
     def agent_specs(self, role):
         """Return agent specs for a particular role."""
         specs = {
-            'DMLC_ROLE': role, 'DMLC_PS_ROOT_URI': self.scheduler['host'],
-            'DMLC_PS_ROOT_PORT': self.scheduler['port'], 'DMLC_NUM_SERVER': self.num_workers,
-            'DMLC_NUM_WORKER': self.num_servers, 'PS_VERBOSE': self.ps_verbose_level
+            'DMLC_ROLE': role,
+            'DMLC_PS_ROOT_URI': self.scheduler['host'], 'DMLC_PS_ROOT_PORT': self.scheduler['port'],
+            'DMLC_NUM_SERVER': self.num_workers, 'DMLC_NUM_WORKER': self.num_servers,
+            'PS_VERBOSE': self.ps_verbose_level
         }
-        if self.scheduler['interface'] is not None:
-            specs['DMLC_INTERFACE'] = self.scheduler['interface']
+        if self.host_ip is not None:
+            specs['DMLC_NODE_HOST'] = self.host_ip
+        elif self.host_interface is not None:
+            specs['DMLC_INTERFACE'] = self.host_interface
         return specs
 
     def run_agent(self, role):
@@ -128,7 +137,7 @@ class Cluster(object):
             if self.agents['scheduler'].poll() is not None:
                 print_now("Scheduler was not started (return code=%s)" % (self.agents['scheduler'].poll()))
                 exit(1)
-        if self.is_servers > 0:
+        if self.is_server > 0:
             self.agents['server'] = self.run_agent('server')
 
         env = os.environ.copy()
@@ -148,13 +157,14 @@ class Cluster(object):
 
         Generally, scheduler and server should shutdown themselves once all workers exited.
         """
-        def _alive(proc):
-            return proc is not None and proc.poll() is None
+        def _alive(role):
+            agent = self.agents.get(role, None)
+            return agent is not None and agent.poll() is None
+
         print_now("Shutting down agents.")
-        if _alive(self.agents['server']):
-            self.agents['server'].terminate()
-        if _alive(self.agents['scheduler']):
-            self.agents['scheduler'].terminate()
+        for agent_role in ('server', 'scheduler'):
+            if _alive(agent_role):
+                self.agents[agent_role].terminate()
 
 
 def main():
